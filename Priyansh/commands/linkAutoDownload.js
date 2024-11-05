fb0799a2781351a1399457379726490b78d1c9a4
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-05 11:37:21 +0530

    Update commands

---
+++ Priyansh/commands/linkAutoDownload.js
@@ -0,0 +1,48 @@
+module.exports = {
+  config: {
+    name: "linkAutoDownload",
+    version: "1.3.0",
+    hasPermssion: 0,
+    credits: "AADI BABU",
+    description:
+      "Automatically detects links in messages and downloads the file.",
+    commandCategory: "Utilities",
+    usages: "",
+    cooldowns: 5,
+  },
+  run: async function ({ events, args }) {},
+  handleEvent: async function ({ api, event, args }) {
+    const axios = require("axios");
+    const request = require("request");
+    const fs = require("fs-extra");
+    const content = event.body ? event.body : "";
+    const body = content.toLowerCase();
+    const { alldown } = require("nayan-media-downloader");
+    if (body.startsWith("https://")) {
+      api.setMessageReaction("🩷", event.messageID, (err) => {}, true);
+      const data = await alldown(content);
+      console.log(data);
+      const { low, high, title } = data.data;
+      api.setMessageReaction("🖤", event.messageID, (err) => {}, true);
+      const video = (
+        await axios.get(high, {
+          responseType: "arraybuffer",
+        })
+      ).data;
+      fs.writeFileSync(
+        __dirname + "/cache/auto.mp4",
+        Buffer.from(video, "utf-8")
+      );
+
+      return api.sendMessage(
+        {
+          body: `=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 
+__________________________________\n\n\nᴛɪᴛʟᴇ: ${title}\n\n__________________________________`,
+          attachment: fs.createReadStream(__dirname + "/cache/auto.mp4"),
+        },
+        event.threadID,
+        event.messageID
+      );
+    }
+  },
+};
