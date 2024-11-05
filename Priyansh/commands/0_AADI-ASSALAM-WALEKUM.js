210c03dacabe8e548e8de44a94622253346e2567
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-05 16:06:39 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+        name: "BYE",
+    version: "1.1.1",
+        hasPermssion: 0,
+        credits: "AADI BABU", 
+        description: "THIS BOT IS AADI SHARMA",
+        commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        let react = event.body.toLowerCase();
+        if(react.includes("assalamualaikum") ||
+     react.includes("asslam walikum") || react.includes("ASSLAM WALIKUM") || react.includes("Aslam") ||
+react.includes("Assalam alaikum") ||
+react.includes("assalamualaikum")) {
+                var msg = {
+                                body: `____________________________________\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n____________________________________ \n𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮`,
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
