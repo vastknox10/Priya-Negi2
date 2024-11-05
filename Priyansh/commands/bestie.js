13b879a8f2d96b3d2b512a65e38b95dcfb4788dc
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-05 19:36:52 +0530

    Update bestie.js

--- Priyansh/commands/bestie.js
+++ Priyansh/commands/bestie.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'bestu.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/RloX16v.jpg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.imgur.com/r4Q3T04.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
