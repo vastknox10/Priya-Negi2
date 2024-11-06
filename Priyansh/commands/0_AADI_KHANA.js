dcb393d6492656b407b720902b9ba90f0e948926
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-06 09:45:56 +0530

    Update 0_AADI_KHANA.js

--- Priyansh/commands/0_AADI_KHANA.js
+++ Priyansh/commands/0_AADI_KHANA.js
@@ -1,28 +1,28 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "KHANA",
+        name: "KISS-YOU",
     version: "1.1.1",
-	hasPermssion: 0,
-	credits: "AADI BABU",
-	description: "THIS BOT IS MR AADI SHARMA",
-	commandCategory: "no prefix",
+        hasPermssion: 0,
+        credits: "AADI BABU",
+        description: "THIS BOT IS MR AADI SHARMA",
+        commandCategory: "no prefix",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("KHANA") ||
-     react.includes("khana") || react.includes("thana") || react.includes("खाना") ||
-react.includes("kuch khaya") ||
-react.includes("kha lo")) {
-		var msg = {
-				body: `𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n🩷 𝐋𝐨 𝐁𝐚𝐁𝐲 \n__________________________________`,attachment: fs.createReadStream(__dirname + `/noprefix/received_472683075271350.mp4`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        let react = event.body.toLowerCase();
+        if(react.includes("kiss") ||
+     react.includes("kiss you") || react.includes("umma") || react.includes("KISS DO") ||
+react.includes("kiss me") ||
+react.includes("KISS YOU")) {
+                var msg = {
+                                body: `=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐋𝐨 𝐁𝐚𝐁𝐲\n__________________________________`,attachment: fs.createReadStream(__dirname + `/noprefix/received_472683075271350.mp4`)
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
