eb2ab23df9fa9f9b002c4898f85d47787354ab1e
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-09 20:35:57 +0530

    Update 0_AADI_LOVE-YOU.js

--- Priyansh/commands/0_AADI_LOVE-YOU.js
+++ Priyansh/commands/0_AADI_LOVE-YOU.js
@@ -1,4 +1,4 @@
-//@Prem-babu3608
+//@aadi-babu
 ////////////////////////////////////////////////////////
 /////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
@@ -6,7 +6,7 @@ module.exports.config = {
 	name: "lOVE-YOU",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", ////////////@prem-babu3608
+	credits: "AADI BABU", ////////////@aadi-babu
 	description: "THIS BOT IS MR PREM SHARMA",
 	commandCategory: "no prefix",
     cooldowns: 5, 
@@ -15,10 +15,10 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("Love") ||
-     react.includes("I love") || react.includes("lOve") || react.includes("LoVe") ||
-react.includes("love") ||
-react.includes("Love")) {
+	if(react.includes("Love you") ||
+     react.includes("LOVE YOU") || react.includes("love you") || react.includes("I love") ||
+react.includes("I LOVE") ||
+react.includes("i love")) {
 		var msg = {
 				body: `😘 𝐌𝐄𝐑𝐀 𝐁𝐀𝐁𝐘 𝐈 𝐋𝐎𝐕𝐄 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 😘`,attachment: fs.createReadStream(__dirname + `/noprefix/AADI123.jpg`)
 			}
