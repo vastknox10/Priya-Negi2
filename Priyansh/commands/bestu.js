10622387de82389326cf2a3f94e4925be6f82972
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-11 13:09:43 +0530

    Update bestu.js

--- Priyansh/commands/bestu.js
+++ Priyansh/commands/bestu.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(160, 160), 105, 111).composite(circleTwo.resize(160, 160), 470, 111);
+  batgiam_img.composite(circleOne.resize(160, 160), 108, 108).composite(circleTwo.resize(160, 160), 450, 108);
 
   let raw = await batgiam_img.getBufferAsync("image/png");
 
