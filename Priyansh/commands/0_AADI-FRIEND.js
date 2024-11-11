d2a70add1f49ed5bf044406f9e3768bd309425e1
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-11-11 13:27:48 +0530

    Update 0_AADI-FRIEND.js

--- Priyansh/commands/0_AADI-FRIEND.js
+++ Priyansh/commands/0_AADI-FRIEND.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'Bbro.png1');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/BfY2u0o.jpg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.imgur.com/b07YKTY.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
