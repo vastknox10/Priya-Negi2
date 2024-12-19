27bdeabee33608d3071f28893ac950ae7803da39
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 04:24:16 +0530

    Update 0_AADI-POPCORN.js

--- Priyansh/commands/0_AADI-POPCORN.js
+++ Priyansh/commands/0_AADI-POPCORN.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("POPCORN")==0 || event.body.indexOf("popcorn")==0 || event.body.indexOf("Popcorn")==0 || event.body.indexOf("popcon")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐨𝐏𝐜𝐎𝐧\n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐨𝐏𝐜𝐎𝐧\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pin_by_Marii_Bernardes_on_Netflix_[Video]_|_Food_snapchat,_Food_vids,_Instagram_food(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
