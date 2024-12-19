9e627ba91971190030c430eba1a6d4bf2779bfc8
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 05:06:03 +0530

    Update 0_AADI_KISS-YOU.js

--- Priyansh/commands/0_AADI_KISS-YOU.js
+++ Priyansh/commands/0_AADI_KISS-YOU.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.config = {
-                name: "miss-you",
+                name: "kiss-you",
                 version: "1.0.1",
                 hasPermssion: 0,
                 credits: "AADI",
@@ -15,35 +15,35 @@ module.exports.config = {
 };
 
 const gifs = [
-                "https://i.imgur.com/OKrKL2C.gif",
-                "https://i.imgur.com/zxBWsmb.gif",
-                "https://i.imgur.com/QmVz2iO.gif",
-                "https://i.imgur.com/pxegLjj.gif",
-                "https://i.imgur.com/OW736Wt.gif"
+                "https://i.imgur.com/ydSH5c6.gif",
+                "https://i.imgur.com/gWONMKL.gif",
+                "https://i.imgur.com/8NdZubK.gif",
+                "https://i.imgur.com/vFiSyAs.gif",
+                "https://i.imgur.com/N74hjd0.gif"
 ];
 
 const messages = [
-                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n𝐌𝐄𝐑 𝐁𝐀𝐁𝐘 𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 🥺\n",
-                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n𝐌𝐄𝐑 𝐁𝐀𝐁𝐘 𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 🥺\n",
-                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n𝐌𝐄𝐑 𝐁𝐀𝐁𝐘 𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 🥺\n",
-                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n𝐌𝐄𝐑 𝐁𝐀𝐁𝐘 𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 🥺\n",
-                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n𝐌𝐄𝐑 𝐁𝐀𝐁𝐘 𝐈 𝐌𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎𝐎 𝐉𝐀𝐀𝐍𝐔 🥺\n"
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n🩷 🖤 𝐊𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 𝐉𝐀𝐋𝐃𝐈 𝐉𝐀𝐋𝐃𝐈 𝐊𝐀𝐑 𝐋𝐎 𝐊𝐎𝐈 𝐃𝐀𝐊𝐇 𝐋𝐄𝐆𝐀 😁 💋🙊💞\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n🩷 🖤 𝐊𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 𝐉𝐀𝐋𝐃𝐈 𝐉𝐀𝐋𝐃𝐈 𝐊𝐀𝐑 𝐋𝐎 𝐊𝐎𝐈 𝐃𝐀𝐊𝐇 𝐋𝐄𝐆𝐀 😁 💋🙊💞\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n🩷 🖤 𝐊𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 𝐉𝐀𝐋𝐃𝐈 𝐉𝐀𝐋𝐃𝐈 𝐊𝐀𝐑 𝐋𝐎 𝐊𝐎𝐈 𝐃𝐀𝐊𝐇 𝐋𝐄𝐆𝐀 😁 💋🙊💞\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n🩷 🖤 𝐊𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 𝐉𝐀𝐋𝐃𝐈 𝐉𝐀𝐋𝐃𝐈 𝐊𝐀𝐑 𝐋𝐎 𝐊𝐎𝐈 𝐃𝐀𝐊𝐇 𝐋𝐄𝐆𝐀 😁 💋🙊💞\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n🩷 🖤 𝐊𝐈𝐒𝐒 𝐘𝐎𝐔 𝐓𝐎 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 𝐉𝐀𝐋𝐃𝐈 𝐉𝐀𝐋𝐃𝐈 𝐊𝐀𝐑 𝐋𝐎 𝐊𝐎𝐈 𝐃𝐀𝐊𝐇 𝐋𝐄𝐆𝐀 😁 💋🙊💞\n"
 ];
 
 module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
                 var { threadID, messageID } = event;
                 var name = await Users.getNameUser(event.senderID);
 
-                if (event.body.toLowerCase().startsWith("MISS YOU") || 
-                                event.body.toLowerCase().startsWith("miss you") || 
-                                event.body.toLowerCase().startsWith("Miss you") || 
-                                event.body.toLowerCase().startsWith("Miss me") || 
-                                event.body.toLowerCase().startsWith("miss me")) { 
+                if (event.body.toLowerCase().startsWith("KISS YOU") || 
+                                event.body.toLowerCase().startsWith("kiss you") || 
+                                event.body.toLowerCase().startsWith("Kiss you") || 
+                                event.body.toLowerCase().startsWith("Kiss me") || 
+                                event.body.toLowerCase().startsWith("kiss me")) { 
 
                                 // Select random GIF and message
                                 const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
                                 const randomMessage = messages[Math.floor(Math.random() * messages.length)].replace("{name}", name);
-                                const downloadPath = path.join(__dirname, 'miss-you-Gif-Images.gif');
+                                const downloadPath = path.join(__dirname, 'kiss-you-Gif-Images.gif');
 
                                 // Download image from Imgur
                                 request(randomGif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
@@ -52,7 +52,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
                                                                 attachment: fs.createReadStream(downloadPath)
                                                 };
                                                 api.sendMessage(msg, threadID, messageID);
-                                                api.setMessageReaction("🥺", event.messageID, (err) => {}, true);
+                                                api.setMessageReaction("😘", event.messageID, (err) => {}, true);
                                 });
                 }
 }
