3a6282d05c218570547df9e251627cfb7c6369c2
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 04:23:12 +0530

    Update 0_AADI-PEPSI.js

--- Priyansh/commands/0_AADI-PEPSI.js
+++ Priyansh/commands/0_AADI-PEPSI.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pin_di_Shoaib_nazir_su_Quick_Saves(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
