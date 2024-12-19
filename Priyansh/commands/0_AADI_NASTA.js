9a0a0394578f64ed6a9809e4bfe94e5b5228db99
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 05:08:23 +0530

    Update 0_AADI_NASTA.js

--- Priyansh/commands/0_AADI_NASTA.js
+++ Priyansh/commands/0_AADI_NASTA.js
@@ -1,26 +1,62 @@
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
 module.exports.config = {
-	name: "nasta",
-    version: "1.0.1",
-	hasPermssion: 0,
-	credits: "FAIZ ANSARI", 
-	description: "hihihihi",
-	commandCategory: "no prefix",
-	usages: "nasta",
-    cooldowns: 5, 
+                name: "nasta-you",
+                version: "1.0.1",
+                hasPermssion: 0,
+                credits: "ARYAN",
+                description: "no prefix",
+        usePrefix: false,
+                commandCategory: "No command marks needed",
+                usages: "Yo Yo",
+                cooldowns: 5,
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("BREAKFAST")==0 || event.body.indexOf("Nasta")==0 || event.body.indexOf("NASTA")==0 || event.body.indexOf("barekfast")==0) {
-		var msg = {
-				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮   😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂",
-				attachment: fs.createReadStream(__dirname + `/noprefix/nasta.png`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😋", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+const gifs = [
+                "https://i.imgur.com/z2EbkKC.gif",
+                "https://i.imgur.com/9HaPHmv.gif",
+                "https://i.imgur.com/StrF9B8.gif",
+                "https://i.imgur.com/GCYkSwa.gif",
+                "https://i.imgur.com/sF5I9gU.gif"
+];
 
-        }
+const messages = [
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂\n",
+                "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n──────────────────\n\n😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂\n"
+];
+
+module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
+                var { threadID, messageID } = event;
+                var name = await Users.getNameUser(event.senderID);
+
+                if (event.body.toLowerCase().startsWith("nasta") || 
+                                event.body.toLowerCase().startsWith("NASTA") || 
+                                event.body.toLowerCase().startsWith("Nasta") || 
+                                event.body.toLowerCase().startsWith("nassta") || 
+                                event.body.toLowerCase().startsWith("Nassta")) { 
+
+                                // Select random GIF and message
+                                const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+                                const randomMessage = messages[Math.floor(Math.random() * messages.length)].replace("{name}", name);
+                                const downloadPath = path.join(__dirname, 'nasta-you-Gif-Images.gif');
+
+                                // Download image from Imgur
+                                request(randomGif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
+                                                var msg = {
+                                                                body: randomMessage,
+                                                                attachment: fs.createReadStream(downloadPath)
+                                                };
+                                                api.sendMessage(msg, threadID, messageID);
+                                                api.setMessageReaction("🤤", event.messageID, (err) => {}, true);
+                                });
+                }
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
