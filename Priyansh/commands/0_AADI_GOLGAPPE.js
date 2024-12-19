287a693324b16292a5c431fb4e2f9ca73706ac02
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 04:52:16 +0530

    Update 0_AADI_GOLGAPPE.js

--- Priyansh/commands/0_AADI_GOLGAPPE.js
+++ Priyansh/commands/0_AADI_GOLGAPPE.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "GOLGAPPE",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "AADI", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "sub",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
-				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 === 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
+				body: "𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷\n\n< ────────────────── >",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Pin_on_Pins_by_you(360P)(1).mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
