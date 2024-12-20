5bfdb710aaf694a2e33f63160a37ad367430173e
shaankhank123001 <shaankhank42@gmail.com>
2024-12-20 05:32:59 +0530

    Update 0_BOSS.js

--- Priyansh/commands/0_BOSS.js
+++ Priyansh/commands/0_BOSS.js
@@ -1,44 +1,28 @@
-const request = require('request');
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
 module.exports.config = {
-    name: "boss",
-    version: "1.0.1",
-    hasPermssion: 0,
-    credits: "SHANKAR SUMAN",
-    description: "no prefix",
-    usePrefix: false,
-    commandCategory: "No command marks needed",
-    usages: "Yo Yo",
-    cooldowns: 5,
+        name: "khana-khao",
+    version: "1.1.1",
+        hasPermssion: 0,
+        credits: "AADI BABU",
+        description: "THIS BOT IS MR AADI BABU",
+        commandCategory: "no prefix",
+    cooldowns: 5, 
 };
 
-const gif = "https://i.imgur.com/ZOZv3qU.jpeg";
-const message = "● ======= 𝐇𝐄𝐋𝐋𝐎 𝐁𝐀𝐁𝐘 ======= ●                                                          ☟  ========== ☟ ==========  ☟.                                                         ●============================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮";
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        let react = event.body.toLowerCase();
+        if(react.includes("Maryam rajput") ||
+     react.includes("MARYAM RAJPUT") || react.includes("Maryam") || react.includes("maryam") ||
+react.includes("MARYAM") ||
+react.includes("@Maryam Rajput")) {
+                var msg = {
+                                body: `𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨 𝐁𝐨𝐭 𝐢𝐃 \n\n● ────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮\n\n● ────────────────── ●\n\n𝐊𝐢𝐬𝐢 𝐁𝐡𝐢 𝐓𝐚𝐇𝐚𝐫𝐚 𝐊𝐢 𝐇𝐞𝐥𝐩 𝐋𝐚𝐧𝐢 𝐇𝐨 𝐌𝐞𝐒𝐒𝐠 𝐊𝐚𝐑𝐞 𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n\nhttps://www.facebook.com/aadi.babu123\n\n● ────────────────── ●\n\n= 𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮`,attachment: fs.createReadStream(__dirname + `/noprefix/Picsart_24-12-13_23-13-49-826.jpg`)
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
-    var { threadID, messageID } = event;
-    const lowerCaseMessage = event.body.toLowerCase();
-
-    if (lowerCaseMessage.startsWith("shan") || 
-        lowerCaseMessage.startsWith("SHAAN") || 
-        lowerCaseMessage.startsWith("shaan")) { 
-
-        const downloadPath = path.join(__dirname, 'Boss-Jpg-Images.jpg');
-
-        // Download image from Imgur
-        request(gif).pipe(fs.createWriteStream(downloadPath)).on('close', () => {
-            var msg = {
-                body: message,
-                attachment: fs.createReadStream(downloadPath)
-            };
-            api.sendMessage(msg, threadID, messageID);
-            api.setMessageReaction("😘", event.messageID, (err) => {}, true);
-        });
-    }
-}
-
-module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-}
+  }
