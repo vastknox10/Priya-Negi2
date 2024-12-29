149dc74367e50565d1eb215ab7dbd22637f99349
brokenaadi143A <brokenaadi143@gmail.com>
2024-12-29 08:35:10 +0530

    Update autosend.js

--- Priyansh/commands/autosend.js
+++ Priyansh/commands/autosend.js
@@ -53,7 +53,7 @@ const imageLinks = [
 
 module.exports.onLoad = ({ api }) => {
   console.log(
-    chalk.bold.hex("60000")(
+    chalk.bold.hex("#00c300")(
       "============ SUCCESFULLY LOADED THE AUTOSENT COMMAND ============"
     )
   );
