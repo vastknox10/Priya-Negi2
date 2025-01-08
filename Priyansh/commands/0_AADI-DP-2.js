4340fa2f962663fae04c97a26df6f225ca5d4507
brokenaadi143A <brokenaadi143@gmail.com>
2025-01-08 18:09:13 +0530

    Update 0_AADI-DP-2.js

--- Priyansh/commands/0_AADI-DP-2.js
+++ Priyansh/commands/0_AADI-DP-2.js
@@ -3,104 +3,81 @@ module.exports.config = {
   version: "1.0.0",
   hasPermssion: 0,
   credits: "AADI BABU",
-  description: "THIS BOT IS MADE BY AADI BABU",
-  commandCategory: "PROFILE DP 2",
-  usages: "SELF DP AND MENTION",
-  dependencies: {
-        "axios": "",
-        "fs-extra": ""
-  },
-  cooldowns: 0
+  description: "dpname maker",
+  commandCategory: "dpname",
+  usages: "text 1 + text 2",
+  cooldowns: 1
 };
- 
-module.exports.wrapText = (ctx, name, maxWidth) => {
-        return new Promise(resolve => {
-                if (ctx.measureText(name).width < maxWidth) return resolve([name]);
-                if (ctx.measureText('W').width > maxWidth) return resolve(null);
-                const words = name.split(' ');
-                const lines = [];
-                let line = '';
-                while (words.length > 0) {
-                        let split = false;
-                        while (ctx.measureText(words[0]).width >= maxWidth) {
-                                const temp = words[0];
-                                words[0] = temp.slice(0, -1);
-                                if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
-                                else {
-                                        split = true;
-                                        words.splice(1, 0, temp.slice(-1));
-                                }
-                        }
-                        if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
-                        else {
-                                lines.push(line.trim());
-                                line = '';
-                        }
-                        if (words.length === 0) lines.push(line.trim());
-                }
-                return resolve(lines);
-        });
-}
- 
-module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
+module.exports.wrapText = (ctx, text, maxWidth) => {
+  return new Promise((resolve) => {
+    if (ctx.measureText(text).width < maxWidth) return resolve([text]);
+    if (ctx.measureText("W").width > maxWidth) return resolve(null);
+    const words = text.split(" ");
+    const lines = [];
+    let line = "";
+    while (words.length > 0) {
+      let split = false;
+      while (ctx.measureText(words[0]).width >= maxWidth) {
+        const temp = words[0];
+        words[0] = temp.slice(0, -1);
+        if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
+        else {
+          split = true;
+          words.splice(1, 0, temp.slice(-1));
+        }
+      }
+      if (ctx.measureText(`${line}${words[0]}`).width < maxWidth)
+        line += `${words.shift()} `;
+      else {
+        lines.push(line.trim());
+        line = "";
+      }
+      if (words.length === 0) lines.push(line.trim());
+    }
+    return resolve(lines);
+  });
+};
+
+module.exports.run = async function ({ api, event, args, Users }) {
+  let { senderID, threadID, messageID } = event;
   const { loadImage, createCanvas } = require("canvas");
+  const Canvas = global.nodemodule["canvas"];
+  const request = require('request');
   const fs = global.nodemodule["fs-extra"];
   const axios = global.nodemodule["axios"];
-  let pathImg = __dirname + "/cache/background.png";
-  let pathAvt1 = __dirname + "/cache/Avtmot.png";
- 
- 
-  var id = Object.keys(event.mentions)[0] || event.senderID;
-  var name = await Users.getNameUser(id);
-  var ThreadInfo = await api.getThreadInfo(event.threadID);
- 
-  var background = [
- 
-    "https://i.imgur.com/JBn1ffv.jpeg"
-];
-  var rd = background[Math.floor(Math.random() * background.length)];
- 
-  let getAvtmot = (
-    await axios.get(
-      `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
- 
-  let getbackground = (
-    await axios.get(`${rd}`, {
+  let pathImg = __dirname + `/cache/drake.png`;
+  const text = args.join(" ").trim().replace(/\s+/g, " ").replace(/(\s+\=)/g, "+").replace(/\|\s+/g, "+").split("+");
+  let getImage = (
+    await axios.get(encodeURI(`https://i.imgur.com/czxzynv.jpeg`), {
       responseType: "arraybuffer",
     })
   ).data;
-  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
- 
+  fs.writeFileSync(pathImg, Buffer.from(getImage, "utf-8"));
+if(!fs.existsSync(__dirname+'/cache/SVN-Arial 2.ttf')) { 
+      let getfont = (await axios.get(`https://drive.google.com/u/0/uc?id=11YxymRp0y3Jle5cFBmLzwU89XNqHIZux&export=download`, { responseType: "arraybuffer" })).data;
+       fs.writeFileSync(__dirname+"/cache/SVN-Arial 2.ttf", Buffer.from(getfont, "utf-8"));
+    };
   let baseImage = await loadImage(pathImg);
-  let baseAvt1 = await loadImage(pathAvt1);
- 
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.font = "400 23px Arial";
-          ctx.fillStyle = "#1878F3";
-          ctx.textAlign = "start";
-         
-         
-          const lines = await this.wrapText(ctx, name, 1160);
-          ctx.fillText(lines.join('\n'), 0,0);//comment
-          ctx.beginPath();
- 
- 
-  ctx.drawImage(baseAvt1, 130, 128, 137, 170);
- 
+  Canvas.registerFont(__dirname+`/cache/SVN-Arial 2.ttf`, {
+        family: "SVN-Arial 2"
+    });
+  ctx.font = "40px SVN-Arial 2";
+  ctx.fillStyle = "#000000";
+  ctx.textAlign = "center";
+  const line = await this.wrapText(ctx, text[0], 400);
+  const lines = await this.wrapText(ctx, text[1], 464);
+  ctx.fillText(line.join("\n"), 230, 282)
+  ctx.fillText(lines.join("\n"), 480, 650)
+  ctx.beginPath();
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
-  fs.removeSync(pathAvt1);
-  return api.sendMessage({ body: ` `, attachment: fs.createReadStream(pathImg) },
-      event.threadID,
-      () => fs.unlinkSync(pathImg),
-      event.messageID);
-    }
- 
- 
-//MADE BY AADI BABU 
+  return api.sendMessage(
+    { attachment: fs.createReadStream(pathImg) },
+    threadID,
+    () => fs.unlinkSync(pathImg),
+    messageID
+  );
+};
