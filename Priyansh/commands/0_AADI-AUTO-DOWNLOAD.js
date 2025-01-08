9bed29b6b2927f795f451a59cb9476180ae4315d
brokenaadi143A <brokenaadi143@gmail.com>
2025-01-08 20:13:19 +0530

    Update 0_AADI-AUTO-DOWNLOAD.js

--- Priyansh/commands/0_AADI-AUTO-DOWNLOAD.js
+++ Priyansh/commands/0_AADI-AUTO-DOWNLOAD.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "linkAutoDownload",
     version: "1.3.0",
     hasPermssion: 0,
-    credits: "ARIF BABU",
+    credits: "AADI BABU",
     description:
       "Automatically detects links in messages and downloads the file.",
     commandCategory: "Utilities",
