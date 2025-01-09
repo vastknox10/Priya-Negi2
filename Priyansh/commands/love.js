4ac5cea03dc8046d238830389a0375cc0345950c
brokenaadi143A <brokenaadi143@gmail.com>
2025-01-09 22:31:30 +0530

    Update love.js

--- Priyansh/commands/love.js
+++ Priyansh/commands/love.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(200, 200), 100, 120).composite(circleTwo.resize(200, 200), 445, 120);
+    batgiam_img.composite(circleOne.resize(180, 180), 100, 110).composite(circleTwo.resize(180, 180), 430, 110);
 
     let raw = await batgiam_img.getBufferAsync("image/jpeg");
 
