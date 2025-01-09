fa367378c213767220cbacc61aadc8fe01778e55
brokenaadi143A <brokenaadi143@gmail.com>
2025-01-09 14:09:11 +0530

    Update pair2.js

--- Priyansh/commands/pair2.js
+++ Priyansh/commands/pair2.js
@@ -41,7 +41,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(258, 258), 120, 110).composite(circleTwo.resize(250, 250), 480, 110);
+    pairing_img.composite(circleOne.resize(258, 258), 117, 110).composite(circleTwo.resize(247, 247), 480, 100);
 
     let raw = await pairing_img.getBufferAsync("image/png");
 
